f149dd7969c5ad1df2a3682d6c90161f60714ef0
Rabin Acharya <rabin7acharya@gmail.com>
2023-08-19 00:50:01 +0545

    nav and hero-section

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".fa-bars").click(function () {
+    $(".ham-menu").slideToggle();
+  });
+
+  $(".fa-xmark").click(function () {
+    $(".ham-menu").slideToggle();
+  });
+});
