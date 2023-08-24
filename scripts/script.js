3551d66eb6d2e1e973f58806eb1c2f4cbfab6658
Rabin Acharya <rabin7acharya@gmail.com>
2023-08-24 19:16:16 +0545

    service overview, inclusion and gallery section

--- scripts/script.js
+++ scripts/script.js
@@ -13,4 +13,18 @@ $(document).ready(function () {
   $(".item-card").click(function () {
     window.location.href = "service.html";
   });
+
+  //tabbed pane
+  $(".tab").click(function () {
+    $(".tab").removeClass("active");
+    $(this).addClass("active");
+
+    var tabIndex = $(this).index();
+    $(".content").removeClass("active");
+    $(".content:eq(" + tabIndex + ")").addClass("active");
+  });
+
+  // Set the initial active tab and content
+  $(".tab:first").addClass("active");
+  $(".content:first").addClass("active");
 });
